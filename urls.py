3d9e3d63e8196fe8266040169fc0ca1fb4df2b22
root <root@ip-10-250-49-172.us-west-2.compute.internal>
2013-09-25 16:50:21 +0000

    update

--- urls.py
+++ urls.py
@@ -1,10 +1,15 @@
 from django.conf.urls.defaults import patterns, include, url
 from jwrfid import views
 
+#from django.contrib import admin
+#admin.autodiscover()
+
 urlpatterns = patterns('',
 
     # Examples:
     url(r'^(?P<_rfid>\d+)/(?P<_type>[a-z]+)$', views.swipe, name='swipe'),
     url(r'^(?P<_type>[a-z]+)/$', views.station, name='station'),
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+
+    #url(r'^admin/', include(admin.site.urls))
 )
